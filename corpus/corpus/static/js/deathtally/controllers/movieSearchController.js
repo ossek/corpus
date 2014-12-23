f87438b9142db3991730b22f1b47c041d7daa4e4
ossek <guy.20.sherlocken@spamgourmet.com>
2014-12-22 23:55:14 -0600

    use right script tags

--- corpus/corpus/static/js/deathtally/controllers/movieSearchController.js
+++ corpus/corpus/static/js/deathtally/controllers/movieSearchController.js
@@ -11,11 +11,14 @@ angular.module('corpus')
   .controller('movieSearchCtrl', ['$scope','movieSearchService',function ($scope,movieSearchService) {
       $scope.searchTerm = "";
       $scope.searchResults = [];
+      $scope.prop = 'WAT';
 
       $scope.clickSearch = function(){
+          console.log('clicked');
           var promise = movieSearchService.searchByTitle($scope.searchTerm);
           promise.then(function(resultData){
               $scope.searchResults = resultData;
+              console.log(resultData);
           },function(reason){
               //todo toast
               console.log(reason);
--- corpus/corpus/static/js/deathtally/services/movieSearchService.js
+++ corpus/corpus/static/js/deathtally/services/movieSearchService.js
@@ -8,26 +8,28 @@
  * Service in the angApp.
  */
 angular.module('corpus')
-  .service('movieSearchService', ['$http','$q',function ($http,$q) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+.service('movieSearchService', [
+        '$http',
+        '$q',
+        function ($http,$q) {
+            console.log('service');
+            function searchByTitle(searchTerm){
 
-      function searchByTitle(searchTerm){
+                return $q(function(resolve,reject){
+                    var uriEncodedSearchTerm = encodeURI(searchTerm);
+                    $http.get('/deathtally/movieSearch?searchTerm=' + uriEncodedSearchTerm).
+                    success(function(data,status,headers,config){
+                        console.log('search method called');
+                        resolve(data);
+                    }).
+                    error(function(data,status,headers,config){
+                        reject(data);
+                    });
+                });
+            }
 
-          return $q(function(resolve,reject){
-              var uriEncodedSearchTerm = encodeURI(searchTerm);
-              $http.get('/deathtally/movieSearch?searchTerm=' + uriEncodedSearchTerm).
-              success(function(data,status,headers,config){
-                  resolve(data);
-                }).
-              error(function(data,status,headers,config){
-                  reject(data);
-              });
-              
-          });
-      }
+            return {
+                searchByTitle: searchByTitle
+            };
 
-      return {
-          searchByTitle: searchByTitle
-      };
-    
-  }]);
+        }]);
